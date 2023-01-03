8a94d7e82e9a1515e3b7daed11d97329ce235fd5
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-01-03 10:57:00 -0500

    commit tree 4be455af655f57c01754dbe056abc9a2b4919863

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.style.height = "300px";
+element.style.backgroundColor = "#27647B";
+main.remove()
+
+
+let contents = newHeader.innerHTML;
+const main = document.getElementById("<h1>");
+main.innerHTML =
+  "<h1>Robert is the champion</h1>";
